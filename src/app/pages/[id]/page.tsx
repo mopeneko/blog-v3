91620941d559d98ae7dedebe367ea9a89e71bfaf
mopeneko <mopeneko@lem0n.cc>
2025-05-07 15:55:45 +0900

    Page コンポーネントの処理をページに移動

--- src/app/pages/[id]/page.tsx
+++ src/app/pages/[id]/page.tsx
@@ -1,6 +1,7 @@
-import { Page as PageComponent } from '@/components/Page';
 import { fetchPageBySlug } from '@/lib/api/list_posts';
 import { Metadata } from 'next';
+import NextImage from 'next/image';
+import React from 'react';
 
 export async function generateMetadata({
   params,
@@ -33,18 +34,45 @@ export async function generateMetadata({
   };
 }
 
+const formatter = new Intl.DateTimeFormat('ja-JP', {
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit',
+});
+
 export default async function Page({
   params,
 }: { params: Promise<{ id: string }> }) {
   const page = await fetchPageBySlug((await params).id);
 
   return (
-    <PageComponent
-      title={page.title}
-      publishedAt={page.published_at}
-      updatedAt={page.updated_at}
-      content={page.content}
-      thumbnail={page.thumbnail || undefined}
-    />
+    <article className="max-w-lg mx-auto px-2 flex flex-col gap-y-4">
+      <h1 className="text-2xl font-bold">{page.title}</h1>
+
+      <div className="grid grid-row-1 gap-y-2">
+        <div className="text-xs text-right">
+          <div>
+            Published on {formatter.format(new Date(page.published_at))}
+          </div>
+          <div>Updated on {formatter.format(new Date(page.updated_at))}</div>
+        </div>
+      </div>
+
+      {page.thumbnail && (
+        <NextImage
+          className="w-full rounded-lg"
+          src={page.thumbnail.src}
+          alt={page.thumbnail.altText}
+          width={page.thumbnail.width}
+          height={page.thumbnail.height}
+          loading="eager"
+        />
+      )}
+
+      <div
+        className="prose w-full m-auto"
+        dangerouslySetInnerHTML={{ __html: page.content }}
+      />
+    </article>
   );
 }
