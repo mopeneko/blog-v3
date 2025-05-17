07d082ad5f304382fdc59826aa3e0ff32eaa79e9
mopeneko <mopeneko@lem0n.cc>
2025-05-17 17:14:22 +0900

    Vitestをインストール

---
+++ vitest.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { loadEnv } from 'vite';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './vitest.setup.ts',
+    env: loadEnv('test', process.cwd(), '')
+  },
+});
